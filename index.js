b59419ad367ea7c45d082cb53ff932a3127ebf7e
tao.xu <tao.xu>
2019-06-17 11:28:56 +0800

    提交

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const bodyParser = require('koa-body');
 app.use(bodyParser());
 app.use(router.routes());
 
+// class A {
+//     get a(){
+//         console.log('实例方法')
+//     }
+//     async success(){
+//         console.log('实例方法成功')
+//     }
+// }
 
+// class B extends A {
+    
+// }
+
+// new B().a
+// new B().success()
 app.listen(3000);
 console.log('[demo] start-quick is starting at port http://127.0.0.1:3000')
